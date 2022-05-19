2277350fd24401819b6e85989b75bbec480a2ff6
王子荀 <wzx20020111@wangzixundeMacBook-Air.local>
2022-05-19 14:07:51 +0800

    update

--- P1648 炼丹术.cpp
+++ P1648 炼丹术.cpp
@@ -20,3 +20,55 @@
 样例输出
 
 1 */
+
+#include <iostream>
+
+using namespace std;
+
+const int MAXN = 1e6 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 998244353;
+
+int pre[MAXN], a[MAXN];
+
+int find(int x)
+{
+    while (x != pre[x])
+        x = pre[x] = pre[pre[x]];
+    return x;
+}
+
+int pow(int n)
+{
+    int ans = 1, base = 2;
+    for (int i = 1; i <= n; ++i)
+    {
+        ans = (ans * base) % mod;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+        pre[i] = i;
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        int u = find(i), v = find(a[i]);
+        if (u != v)
+            pre[u] = v;
+    }
+    int cnt = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (pre[i] == i)
+            cnt++;
+    }
+    cout << pow(cnt) - 1 << endl;
+    return 0;
+}
