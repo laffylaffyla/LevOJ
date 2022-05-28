c29cc58c347aa7b0cf7e631cb50b10519d957c7a
王子荀 <wzx20020111@wangzixundeMacBook-Air.local>
2022-05-28 21:15:49 +0800

    update

--- P1440 背包九讲(2)：0-1背包的空间优化.cpp
+++ P1440 背包九讲(2)：0-1背包的空间优化.cpp
@@ -1,36 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct item
 {
-	int V,W;
-}T[20001];
+	int V, W;
+} T[20001];
 
-long long Max(long long a,long long b)
+long long Max(long long a, long long b)
 {
-	return a>b?a:b;
+	return a > b ? a : b;
 }
 
 int main()
 {
-	int n,c;
-	while(scanf("%d",&c)!=EOF)
+	int n, c;
+	while (scanf("%d", &c) != EOF)
 	{
-		scanf("%d",&n);
-		long long dp[10001][2]={{0}};
-		for(int i=1;i<=n;i++) 
+		scanf("%d", &n);
+		long long dp[10001][2] = {{0}};
+		for (int i = 1; i <= n; i++)
 		{
-			cin>>T[i].V>>T[i].W;
-			for(int j=1;j<=c;j++)
+			cin >> T[i].V >> T[i].W;
+			for (int j = 1; j <= c; j++)
 			{
-				if(j>=T[i].V)
+				if (j >= T[i].V)
 				{
-					dp[j][1]=Max(dp[j][0],dp[j-T[i].V][0]+T[i].W);
+					dp[j][1] = Max(dp[j][0], dp[j - T[i].V][0] + T[i].W);
 				}
-				else dp[j][1]=dp[j][0];
+				else
+					dp[j][1] = dp[j][0];
 			}
-			for(int j=1;j<=c;j++) dp[j][0]=dp[j][1];
+			for (int j = 1; j <= c; j++)
+				dp[j][0] = dp[j][1];
 		}
-		cout<<dp[c][1]<<endl;
+		cout << dp[c][1] << endl;
 	}
 }
